edcaa811fd18ee015fb32f50ddec8faad04558fc
Sophia Dai <sophiadai8@gmail.com>
2023-09-18 20:21:27 -0700

    First milestone
    1. BLE advertise as HID
    2. IMU worked
    3. TensorFlow inference worked

---
+++ main.ino
@@ -0,0 +1,190 @@
+#include <LSM6DS3.h>
+#include <Wire.h>
+#include <bluefruit.h>
+
+#include <TensorFlowLite.h>
+#include <tensorflow/lite/micro/all_ops_resolver.h>
+#include <tensorflow/lite/micro/micro_error_reporter.h>
+#include <tensorflow/lite/micro/micro_interpreter.h>
+#include <tensorflow/lite/schema/schema_generated.h>
+//#include <tensorflow/lite/version.h>
+
+#include "model.h"
+
+const float accelerationThreshold = 2.5; // threshold of significant in G's
+const int numSamples = 119;
+
+int samplesRead = numSamples;
+
+BLEDis bledis;
+BLEHidAdafruit blehid;
+
+LSM6DS3 myIMU(I2C_MODE, 0x6A);  
+
+// global variables used for TensorFlow Lite (Micro)
+tflite::MicroErrorReporter tflErrorReporter;
+
+// pull in all the TFLM ops, you can remove this line and
+// only pull in the TFLM ops you need, if would like to reduce
+// the compiled size of the sketch.
+tflite::AllOpsResolver tflOpsResolver;
+
+const tflite::Model* tflModel = nullptr;
+tflite::MicroInterpreter* tflInterpreter = nullptr;
+TfLiteTensor* tflInputTensor = nullptr;
+TfLiteTensor* tflOutputTensor = nullptr;
+
+// Create a static memory buffer for TFLM, the size may need to
+// be adjusted based on the model you are using
+constexpr int tensorArenaSize = 8 * 1024;
+byte tensorArena[tensorArenaSize] __attribute__((aligned(16)));
+
+// array to map gesture index to a name
+const char* GESTURES[] = {
+  "punch",
+  "flex"
+};
+
+#define NUM_GESTURES (sizeof(GESTURES) / sizeof(GESTURES[0]))
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (myIMU.begin() != 0) {
+    Serial.println("Device error");
+  } else {
+    Serial.println("Device OK!");
+  }
+
+  Serial.println();
+
+  // get the TFL representation of the model byte array
+  tflModel = tflite::GetModel(model);
+  if (tflModel->version() != TFLITE_SCHEMA_VERSION) {
+    Serial.println("Model schema mismatch!");
+    while (1);
+  }
+
+  // Create an interpreter to run the model
+  tflInterpreter = new tflite::MicroInterpreter(tflModel, tflOpsResolver, tensorArena, tensorArenaSize, &tflErrorReporter);
+
+  // Allocate memory for the model's input and output tensors
+  tflInterpreter->AllocateTensors();
+
+  // Get pointers for the model's input and output tensors
+  tflInputTensor = tflInterpreter->input(0);
+  tflOutputTensor = tflInterpreter->output(0);
+
+    Bluefruit.begin();
+  // HID Device can have a min connection interval of 9*1.25 = 11.25 ms
+  Bluefruit.Periph.setConnInterval(9, 16);  // min = 9*1.25=11.25 ms, max = 16*1.25=20ms
+  Bluefruit.setTxPower(4);                  // Check bluefruit.h for supported values
+
+  // Configure and Start Device Information Service
+  bledis.setManufacturer("Adafruit Industries");
+  bledis.setModel("Bluefruit Feather 52");
+  bledis.begin();
+
+  // BLE HID
+  blehid.begin();
+
+  // Set up and start advertising
+  startAdv();
+}
+
+
+void startAdv(void) {
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_GENERIC_HID);
+
+  // Include BLE HID service
+  Bluefruit.Advertising.addService(blehid);
+
+  // There is enough room for 'Name' in the advertising packet
+  Bluefruit.Advertising.addName();
+
+  /* Start Advertising
+   * - Enable auto advertising if disconnected
+   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+   * - Timeout for fast mode is 30 seconds
+   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+   * 
+   * For recommended advertising interval
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
+   */
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);  // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);    // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);              // 0 = Don't stop advertising after n seconds
+}
+
+
+void loop() {
+  float aX, aY, aZ, gX, gY, gZ;
+  Serial.println("loop");
+blehid.mouseMove(20, 50);
+
+  // wait for significant motion
+  while (samplesRead == numSamples) {
+      // read the acceleration data
+      aX = myIMU.readFloatAccelX();
+      aY = myIMU.readFloatAccelY();
+      aZ = myIMU.readFloatAccelZ();
+
+      // sum up the absolutes
+      float aSum = fabs(aX) + fabs(aY) + fabs(aZ);
+
+      // check if it's above the threshold
+      if (aSum >= accelerationThreshold) {
+        // reset the sample read count
+        samplesRead = 0;
+        break;
+      }
+  }
+
+  // check if the all the required samples have been read since
+  // the last time the significant motion was detected
+  while (samplesRead < numSamples) {
+    // check if new acceleration AND gyroscope data is available
+      // read the acceleration and gyroscope data
+      aX = myIMU.readFloatAccelX();
+      aY = myIMU.readFloatAccelY();
+      aZ = myIMU.readFloatAccelZ();
+
+      gX = myIMU.readFloatGyroX();
+      gY = myIMU.readFloatGyroY();
+      gZ = myIMU.readFloatGyroZ();
+
+      // normalize the IMU data between 0 to 1 and store in the model's
+      // input tensor
+      tflInputTensor->data.f[samplesRead * 6 + 0] = (aX + 4.0) / 8.0;
+      tflInputTensor->data.f[samplesRead * 6 + 1] = (aY + 4.0) / 8.0;
+      tflInputTensor->data.f[samplesRead * 6 + 2] = (aZ + 4.0) / 8.0;
+      tflInputTensor->data.f[samplesRead * 6 + 3] = (gX + 2000.0) / 4000.0;
+      tflInputTensor->data.f[samplesRead * 6 + 4] = (gY + 2000.0) / 4000.0;
+      tflInputTensor->data.f[samplesRead * 6 + 5] = (gZ + 2000.0) / 4000.0;
+
+      samplesRead++;
+
+      if (samplesRead == numSamples) {
+        // Run inferencing
+        TfLiteStatus invokeStatus = tflInterpreter->Invoke();
+        if (invokeStatus != kTfLiteOk) {
+          Serial.println("Invoke failed!");
+          while (1);
+          return;
+        }
+
+        // Loop through the output tensor values from the model
+        for (int i = 0; i < NUM_GESTURES; i++) {
+          Serial.print(GESTURES[i]);
+          Serial.print(": ");
+          Serial.println(tflOutputTensor->data.f[i], 6);
+        }
+        Serial.println();
+      }
+  }
+}
