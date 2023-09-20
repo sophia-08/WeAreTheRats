67bf0c1ca9f9d94b8d4097c46af5dc1692e09d00
Sophia Dai <sophiadai8@gmail.com>
2023-09-19 23:49:17 -0700

    Analyze IMU performance
    Use two methoes to calculate roll/pitch/yaw.

--- WeAreTheRats.ino
+++ WeAreTheRats.ino
@@ -1,4 +1,4 @@
-#include <LSM6DS3.h>
+#include "LSM6DS3.h"
 #include <Wire.h>
 #include <bluefruit.h>
 #include <MadgwickAHRS.h>  // Madgwick 1.2.0 by Arduino
@@ -21,10 +21,21 @@ BLEDis bledis;
 BLEHidAdafruit blehid;
 
 LSM6DS3 myIMU(I2C_MODE, 0x6A);
-Madgwick filter;  // Madgwick filter
-float roll, pitch, yaw;             // attitude
-float aX, aY, aZ, gX, gY, gZ;
-uint8_t readData;                   // for reading IMU register
+Madgwick filter;                                            // Madgwick filter
+float accelX, accelY, accelZ,                               // units m/s/s i.e. accelZ if often 9.8 (gravity)
+  gyroX, gyroY, gyroZ,                                      // units dps (degrees per second)
+  gyroDriftX, gyroDriftY, gyroDriftZ,                       // units dps
+  gyroRoll, gyroPitch, gyroYaw,                             // units degrees (expect major drift)
+  gyroCorrectedRoll, gyroCorrectedPitch, gyroCorrectedYaw,  // units degrees (expect minor drift)
+  accRoll, accPitch, accYaw,                                // units degrees (roll and pitch noisy, yaw not possible)
+  complementaryRoll, complementaryPitch, complementaryYaw;  // units degrees (excellent roll, pitch, yaw minor drift)
+
+long lastTime;
+long lastInterval;
+uint8_t readData;
+
+float roll, pitch, yaw;
+
 
 // global variables used for TensorFlow Lite (Micro)
 tflite::MicroErrorReporter tflErrorReporter;
@@ -59,8 +70,8 @@ void setup() {
 
   // initialize and set IMU
   // refer to   LSM6D3.cpp:351
-  myIMU.settings.gyroRange = 2000;  // calcGyro()
-  myIMU.settings.accelRange = 4;    // calcAccel()
+  // myIMU.settings.gyroRange = 2000;  // calcGyro()
+  // myIMU.settings.accelRange = 4;    // calcAccel()
   if (myIMU.begin() != 0) {
     Serial.println("IMU Device error");
     while (1)
@@ -75,7 +86,7 @@ void setup() {
   // myIMU.writeRegister(LSM6DS3_ACC_GYRO_CTRL8_XL, 0x09);  // ODR/4
 
   // Maadgwick filter sampling rate
-  filter.begin(12.5);
+  filter.begin(416);
 
   // get the TFL representation of the model byte array
   tflModel = tflite::GetModel(model);
@@ -101,8 +112,8 @@ void setup() {
   Bluefruit.setTxPower(4);                  // Check bluefruit.h for supported values
   Bluefruit.setName("WeAreTheRats");
 
-    // Configure and Start Device Information Service
-    bledis.setManufacturer("Adafruit Industries");
+  // Configure and Start Device Information Service
+  bledis.setManufacturer("Adafruit Industries");
   bledis.setModel("Bluefruit Feather 52");
   bledis.begin();
 
@@ -111,8 +122,75 @@ void setup() {
 
   // Set up and start advertising
   startAdv();
+
+  calibrateIMU(250, 250);
+  lastTime = micros();
 }
 
+
+/*
+  the gyro's x,y,z values drift by a steady amount. if we measure this when arduino is still
+  we can correct the drift when doing real measurements later
+*/
+void calibrateIMU(int delayMillis, int calibrationMillis) {
+
+  int calibrationCount = 0;
+
+  delay(delayMillis);  // to avoid shakes after pressing reset button
+
+  float sumX, sumY, sumZ;
+  int startTime = millis();
+  while (millis() < startTime + calibrationMillis) {
+    if (readIMU()) {
+      // in an ideal world gyroX/Y/Z == 0, anything higher or lower represents drift
+      sumX += gyroX;
+      sumY += gyroY;
+      sumZ += gyroZ;
+      calibrationCount++;
+    }
+  }
+
+  if (calibrationCount == 0) {
+    Serial.println("Failed to calibrate");
+  }
+
+  gyroDriftX = sumX / calibrationCount;
+  gyroDriftY = sumY / calibrationCount;
+  gyroDriftZ = sumZ / calibrationCount;
+
+  // {
+  //   Serial.print(calibrationCount);
+  //   Serial.print(",");
+  //   Serial.print(gyroDriftX);
+  //   Serial.print(",");
+  //   Serial.print(gyroDriftY);
+  //   Serial.print(",");
+  //   Serial.print(gyroDriftZ);
+  //   Serial.println("");
+  // }
+
+  // uint8_t* data = myIMU.dumpRegisters();
+  // for (int i = 0; i < 15; i++) {
+  //   Serial.print(i);
+  //   Serial.print(",");
+  //   Serial.println(data[i], HEX);
+  // }
+  // while (1)
+  //   ;
+}
+
+/**
+   Read accel and gyro data.
+   returns true if value is 'new' and false if IMU is returning old cached data
+*/
+// bool readIMU() {
+//   if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() ) {
+//     IMU.readAcceleration(accelX, accelY, accelZ);
+//     IMU.readGyroscope(gyroX, gyroY, gyroZ);
+//     return true;
+//   }
+//   return false;
+// }
 
 void startAdv(void) {
   // Advertising packet
@@ -141,7 +219,7 @@ void startAdv(void) {
   Bluefruit.Advertising.start(0);              // 0 = Don't stop advertising after n seconds
 }
 
-void readIMU() {
+bool readIMU() {
   // wait for IMU data to become valid
   // sample rate is 12.5Hz, so can read every 80mS
   do {
@@ -150,77 +228,153 @@ void readIMU() {
 
   // digitalWrite(LED_RED, LOW);   // data read and send task indicator ON
 
-  aX = myIMU.readFloatAccelX();  // Accel data
-  aY = myIMU.readFloatAccelY();
-  aZ = myIMU.readFloatAccelZ();
-  gX = myIMU.readFloatGyroX();  // Gyro data
-  gY = myIMU.readFloatGyroY();
-  gZ = myIMU.readFloatGyroZ();
+  uint8_t data[12];
+  myIMU.readRegisterRegion(data, 0x22, 12);
+  int16_t* p=( int16_t*)data;
+  gyroX = *p * 2000.0  / 32768.0; p++;
+  gyroY = *p * 2000.0  / 32768.0; p++;
+  gyroZ = *p * 2000.0  / 32768.0; p++;
+  accelX = *p * 4.0 / 32768.0; p++;
+  accelY = *p * 4.0 / 32768.0; p++;
+  accelZ = *p * 4.0 / 32768.0; p++;
+  // Serial.print(data[0],HEX);Serial.print(",");Serial.print(data[1],HEX);Serial.print(",");Serial.println(gyroX);
+  // Serial.print(data[6],HEX);Serial.print(",");Serial.print(data[7],HEX);Serial.print(",");Serial.println(accelX);
+  //   accelX = myIMU.readFloatAccelX();  // Accel data
+  //   accelY = myIMU.readFloatAccelY();
+  //   accelZ = myIMU.readFloatAccelZ();
+  //   gyroX = myIMU.readFloatGyroX();  // Gyro data
+  //   gyroY = myIMU.readFloatGyroY();
+  //  gyroZ = myIMU.readFloatGyroZ();
+  return true;
 }
 
 void storeData() {
   // normalize the IMU data between 0 to 1 and store in the model's
   // input tensor
-  tflInputTensor->data.f[samplesRead * 6 + 0] = (aX + 4.0) / 8.0;
-  tflInputTensor->data.f[samplesRead * 6 + 1] = (aY + 4.0) / 8.0;
-  tflInputTensor->data.f[samplesRead * 6 + 2] = (aZ + 4.0) / 8.0;
-  tflInputTensor->data.f[samplesRead * 6 + 3] = (gX + 2000.0) / 4000.0;
-  tflInputTensor->data.f[samplesRead * 6 + 4] = (gY + 2000.0) / 4000.0;
-  tflInputTensor->data.f[samplesRead * 6 + 5] = (gZ + 2000.0) / 4000.0;
+  tflInputTensor->data.f[samplesRead * 6 + 0] = (accelX + 4.0) / 8.0;
+  tflInputTensor->data.f[samplesRead * 6 + 1] = (accelY + 4.0) / 8.0;
+  tflInputTensor->data.f[samplesRead * 6 + 2] = (accelZ + 4.0) / 8.0;
+  tflInputTensor->data.f[samplesRead * 6 + 3] = (gyroX + 2000.0) / 4000.0;
+  tflInputTensor->data.f[samplesRead * 6 + 4] = (gyroY + 2000.0) / 4000.0;
+  tflInputTensor->data.f[samplesRead * 6 + 5] = (gyroZ + 2000.0) / 4000.0;
 }
 
 void loop() {
 
   // Serial.println("loop");
-  blehid.mouseMove(20, 50);
+  // blehid.mouseMove(20, 50);
   readIMU();
+
+      long currentTime = micros();
+    lastInterval = currentTime - lastTime; // expecting this to be ~104Hz +- 4%
+    lastTime = currentTime;
+
+    doCalculations();
+    printCalculations();
   // wait for significant motion
-  if (samplesRead == numSamples) {
+  // if (samplesRead == numSamples) {
 
 
-    // sum up the absolutes
-    float aSum = fabs(aX) + fabs(aY) + fabs(aZ);
+  //   // sum up the absolutes
+  //   float aSum = fabs(aX) + fabs(aY) + fabs(aZ);
 
-    // check if it's above the threshold
-    if (aSum >= accelerationThreshold) {
-      // reset the sample read count
-      samplesRead = 0;
+  //   // check if it's above the threshold
+  //   if (aSum >= accelerationThreshold) {
+  //     // reset the sample read count
+  //     samplesRead = 0;
 
-    } else {
-      return;
-    }
-  }
-  storeData();
-        // calculate the attitude with Madgwick filter
-      filter.updateIMU(gX, gY, gZ, aX, aY, aZ);
+  //   } else {
+  //     return;
+  //   }
+  // }
+  // storeData();
+  // calculate the attitude with Madgwick filter
+  filter.updateIMU(gyroX, gyroY, gyroZ, accelX, accelY, accelZ);
 
-      roll = filter.getRoll();    // -180 ~ 180deg
-      pitch = filter.getPitch();  // -180 ~ 180deg
-      yaw = filter.getYaw();      // 0 -3 60deg
+        roll = filter.getRoll();    // -180 ~ 180deg
+        pitch = filter.getPitch();  // -180 ~ 180deg
+        yaw = filter.getYaw() - 180;      // 0 - 360deg
 
+  // Serial.print(roll);Serial.print(",");
+  // Serial.print(pitch);Serial.print(",");
+  // Serial.println(yaw);
   // check if the all the required samples have been read since
   // the last time the significant motion was detected
-  while (samplesRead < numSamples) {
-    samplesRead++;
-    readIMU();
-    storeData();
-  }
+  // while (samplesRead < numSamples) {
+  //   samplesRead++;
+  //   readIMU();
+  //   storeData();
+  // }
 
-  // Run inferencing
-  TfLiteStatus invokeStatus = tflInterpreter->Invoke();
-  if (invokeStatus != kTfLiteOk) {
-    Serial.println("Invoke failed!");
-    while (1)
-      ;
-    return;
-  }
+  // // Run inferencing
+  // TfLiteStatus invokeStatus = tflInterpreter->Invoke();
+  // if (invokeStatus != kTfLiteOk) {
+  //   Serial.println("Invoke failed!");
+  //   while (1)
+  //     ;
+  //   return;
+  // }
 
-  // Loop through the output tensor values from the model
-  for (int i = 0; i < NUM_GESTURES; i++) {
-    Serial.print(GESTURES[i]);
-    Serial.print(": ");
-    Serial.println(tflOutputTensor->data.f[i], 6);
-  }
-  Serial.println();
+  // // Loop through the output tensor values from the model
+  // for (int i = 0; i < NUM_GESTURES; i++) {
+  //   Serial.print(GESTURES[i]);
+  //   Serial.print(": ");
+  //   Serial.println(tflOutputTensor->data.f[i], 6);
+  // }
+  // Serial.println();
+}
 
+/**
+   I'm expecting, over time, the Arduino_LSM6DS3.h will add functions to do most of this,
+   but as of 1.0.0 this was missing.
+*/
+void doCalculations() {
+  accRoll = atan2(accelY, accelZ) * 180 / M_PI;
+  accPitch = atan2(-accelX, sqrt(accelY * accelY + accelZ * accelZ)) * 180 / M_PI;
+
+  float lastFrequency = (float) 1000000.0 / lastInterval;
+  gyroRoll = gyroRoll + (gyroX / lastFrequency);
+  gyroPitch = gyroPitch + (gyroY / lastFrequency);
+  gyroYaw = gyroYaw + (gyroZ / lastFrequency);
+
+  gyroCorrectedRoll = gyroCorrectedRoll + ((gyroX - gyroDriftX) / lastFrequency);
+  gyroCorrectedPitch = gyroCorrectedPitch + ((gyroY - gyroDriftY) / lastFrequency);
+  gyroCorrectedYaw = gyroCorrectedYaw + ((gyroZ - gyroDriftZ) / lastFrequency);
+
+  complementaryRoll = complementaryRoll + ((gyroX - gyroDriftX) / lastFrequency);
+  complementaryPitch = complementaryPitch + ((gyroY - gyroDriftY) / lastFrequency);
+  complementaryYaw = complementaryYaw + ((gyroZ - gyroDriftZ) / lastFrequency);
+
+  complementaryRoll = 0.98 * complementaryRoll + 0.02 * accRoll;
+  complementaryPitch = 0.98 * complementaryPitch + 0.02 * accPitch;
+}
+
+/**
+   This comma separated format is best 'viewed' using 'serial plotter' or processing.org client (see ./processing/RollPitchYaw3d.pde example)
+*/
+void printCalculations() {
+  Serial.print(roll);
+  Serial.print(',');
+  Serial.print(pitch);
+  Serial.print(',');
+  Serial.print(yaw);
+  Serial.print(',');
+  Serial.print(gyroCorrectedRoll);
+  Serial.print(',');
+  Serial.print(gyroCorrectedPitch);
+  Serial.print(',');
+  Serial.print(gyroCorrectedYaw);
+  Serial.print(',');
+  Serial.print(accRoll);
+  Serial.print(',');
+  Serial.print(accPitch);
+  Serial.print(',');
+  Serial.print(accYaw);
+  Serial.print(',');
+  Serial.print(complementaryRoll);
+  Serial.print(',');
+  Serial.print(complementaryPitch);
+  Serial.print(',');
+  Serial.print(complementaryYaw);
+  Serial.println("");
 }
