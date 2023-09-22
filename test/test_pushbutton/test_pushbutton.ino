2289925f6582e2fc85b5e3841e61f61e83de49b6
Sophia Dai <sophiadai8@gmail.com>
2023-09-21 17:57:02 -0700

    test code for touch pad

--- test/test_pushbutton/test_pushbutton.ino
+++ test/test_pushbutton/test_pushbutton.ino
@@ -1,22 +1,25 @@
-
-#include <Arduino.h>
-#include "bluefruit_common.h"
+#include <Wire.h>
+#include "bluefruit.h"
 // the setup function runs once when you press reset or power the board
 void setup() {
 // the pins are defined in bsp package **/packages/**vendor/**hardware/nrf52/*version/variants/boardname file
 // variant.h define the index, variant.cpp has the array.
-
+  Serial.begin(9600);
+  // while (!Serial)
+  //   ;
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);   
   pinMode(23, OUTPUT);   
-   
 
+   
+pinMode(D6, INPUT_PULLUP);
   pinMode(D7, INPUT_PULLUP);
   pinMode(D8, INPUT_PULLUP);
   pinMode(D9, INPUT_PULLUP);
   pinMode(D10, INPUT_PULLUP);
-  
+
+    digitalWrite(D6, HIGH);
   digitalWrite(D7, HIGH);
   digitalWrite(D8, HIGH);
   digitalWrite(D9, HIGH);
@@ -29,5 +32,6 @@ void loop() {
   digitalWrite(LED_BLUE, digitalRead(D7));  
   digitalWrite(LED_RED, digitalRead(D8));  
   digitalWrite(LED_GREEN, digitalRead(D9));  
-  digitalWrite(23, digitalRead(D10));  
+  digitalWrite(23, digitalRead(D6));  
+  Serial.println(digitalRead(D6));
 }
