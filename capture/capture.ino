95024c681197cc63ee4f6f08d7291d8b6a71b5f2
Sophia Dai <sophiadai8@gmail.com>
2023-09-22 18:15:08 -0700

    firmware for capture IMU data

--- capture/capture.ino
+++ capture/capture.ino
@@ -2,42 +2,64 @@
 #include <Wire.h>
 
 // Create a instance of class LSM6DS3
-LSM6DS3 myIMU(I2C_MODE, 0x6A); // I2C device address 0x6A
-float accelX, accelY, accelZ,  // units m/s/s i.e. accelZ if often 9.8 (gravity)
-    gyroX, gyroY, gyroZ;       // units dps (degrees per second)
-
-const float accelerationThreshold = 1.8; // threshold of significant in G's
-const int numSamples = 119;
-int samplesRead = numSamples;
+LSM6DS3 myIMU(I2C_MODE, 0x6A);         // I2C device address 0x6A
+float accelX, accelY, accelZ,          // units m/s/s i.e. accelZ if often 9.8 (gravity)
+  gyroX, gyroY, gyroZ,                 // units dps (degrees per second)
+  gyroDriftX, gyroDriftY, gyroDriftZ;  // units dps
+#define MOUSE_LEFT D9
+#define MOUSE_RIGHT D8
+#define MOUSE_ACTIVATE D6
+const float accelerationThreshold = 1.8;  // threshold of significant in G's
+const int numSamples = 416;
+int samplesRead = 0;
 int startTime, currentTime;
-void setup()
-{
+uint8_t readData;
+void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  while (!Serial)
-    ;
+  Serial.begin(9600);
+  // while (!Serial)
+  //   ;
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  // pinMode(LED_CHARGER, OUTPUT);
+  pinMode(D6, INPUT_PULLUP);
+  pinMode(D7, INPUT_PULLUP);
+  pinMode(D8, INPUT_PULLUP);
+  pinMode(D9, INPUT_PULLUP);
+  pinMode(D10, INPUT_PULLUP);
+
+  digitalWrite(D6, HIGH);
+  digitalWrite(D7, HIGH);
+  digitalWrite(D8, HIGH);
+  digitalWrite(D9, HIGH);
+  digitalWrite(D10, HIGH);
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_BLUE, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  // digitalWrite(LED_CHARGER, LOW);
+
   // Call .begin() to configure the IMUs
-  if (myIMU.begin() != 0)
-  {
+  if (myIMU.begin() != 0) {
     Serial.println("Device error");
     while (1)
       ;
   }
-  Wire1.setClock(400000UL); // SCL 400kHz
+  Wire1.setClock(400000UL);  // SCL 400kHz
   startTime = micros();
+  calibrateIMU(0,0);
 }
-bool readIMU()
-{
+
+void calibrateIMU(int delayMillis, int calibrationMillis) {
+  gyroDriftX = 0.67;
+  gyroDriftY = -1.38;
+  gyroDriftZ = -0.51;
+  return;
+}
+
+bool readIMU() {
   uint8_t readData;
-  // wait for IMU data to become valid
-  // sample rate is 416Hz
-  do
-  {
-    myIMU.readRegister(&readData, LSM6DS3_ACC_GYRO_STATUS_REG); // 0,0,0,0,0,TDA,GDA,XLDA
-  } while ((readData & 0x07) != 0x07);
-
-  // digitalWrite(LED_RED, LOW);   // data read and send task indicator ON
-
   uint8_t data[12];
   myIMU.readRegisterRegion(data, 0x22, 12);
   int16_t *p = (int16_t *)data;
@@ -58,49 +80,50 @@ bool readIMU()
 
 int count = 0;
 float sumX, sumY, sumZ;
-void loop()
-{
-
-  readIMU();
-  count++;
-  sumX += gyroX;
-  sumY += gyroY;
-  sumZ += gyroZ;
-  if (count % 100 == 0)
-  {
-    currentTime = micros();
-    Serial.println(currentTime - startTime);
-    startTime = currentTime;
-    Serial.print(sumX / 100);
-    Serial.print(",");
-    Serial.print(sumY / 100);
-    Serial.print(",");
-    Serial.println(sumZ / 100);
-    sumX = 0;
-    sumY = 0;
-    sumZ = 0;
+void loop() {
+  if (digitalRead(MOUSE_ACTIVATE) == LOW) {
+    return;
   }
-  return;
-  // wait for significant motion
-  if (samplesRead == numSamples)
-  {
-    // sum up the absolutes
-    float aSum = fabs(accelX) + fabs(accelY) + fabs(accelZ);
-
-    // check if it's above the threshold
-    if (aSum < accelerationThreshold)
-    {
-      return;
-    }
-  }
-
+  digitalWrite(LED_BLUE, HIGH);
   samplesRead = 0;
+while (samplesRead < numSamples) {
+wait:
+  myIMU.readRegister(&readData, LSM6DS3_ACC_GYRO_STATUS_REG);  //0,0,0,0,0,TDA,GDA,XLDA
+  if ((readData & 0x07) != 0x07) goto wait;
+  readIMU();
+  // count++;
+  // sumX += gyroX;
+  // sumY += gyroY;
+  // sumZ += gyroZ;
+  // if (count % 100 == 0) {
+  //   currentTime = micros();
+  //   Serial.println(currentTime - startTime);
+  //   startTime = currentTime;
+  //   Serial.print(sumX / 100);
+  //   Serial.print(",");
+  //   Serial.print(sumY / 100);
+  //   Serial.print(",");
+  //   Serial.println(sumZ / 100);
+  //   sumX = 0;
+  //   sumY = 0;
+  //   sumZ = 0;
+  // }
+  // return;
+  // wait for significant motion
+  // if (samplesRead == numSamples) {
+  //   // sum up the absolutes
+  //   float aSum = fabs(accelX) + fabs(accelY) + fabs(accelZ);
+
+  //   // check if it's above the threshold
+  //   if (aSum < accelerationThreshold) {
+  //     return;
+  //   }
+  // }
+
+  
   // check if the all the required samples have been read since
   // the last time the significant motion was detected
-  while (samplesRead < numSamples)
-  {
-    // check if both new acceleration and gyroscope data is
-    // available
+  
     // read the acceleration and gyroscope data
     // print the data in CSV format
     Serial.print(accelX, 3);
@@ -109,22 +132,19 @@ void loop()
     Serial.print(',');
     Serial.print(accelZ, 3);
     Serial.print(',');
-    // Serial.print(gyroX, 3);
-    // Serial.print(',');
-    // Serial.print(gyroY, 3);
-    // Serial.print(',');
-    // Serial.print(gyroZ, 3);
+    Serial.print(gyroX-gyroDriftX, 3);
+    Serial.print(',');
+    Serial.print(gyroY-gyroDriftY, 3);
+    Serial.print(',');
+    Serial.print(gyroZ-gyroDriftZ, 3);
     Serial.println("");
 
     samplesRead++;
-    if (samplesRead == numSamples)
-    {
+    if (samplesRead == numSamples) {
       // add an empty line if it's the last sample
       Serial.println();
-    }
-    else
-    {
-      readIMU();
-    }
+      break;
+    } 
   }
+  digitalWrite(LED_BLUE, LOW);
 }
