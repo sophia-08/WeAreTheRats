19a765795a31660beaa6d165c762addec0ff5e7a
Sophia Dai <sophiadai8@gmail.com>
2023-09-27 22:10:46 -0700

    Use bno055, capture 150 sample at interval 10ms

--- capture/capture.ino
+++ capture/capture.ino
@@ -1,12 +1,14 @@
-#include "LSM6DS3.h"
+// #include "LSM6DS3.h"
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BNO055.h"
 
 // Create a instance of class LSM6DS3
-LSM6DS3 myIMU(I2C_MODE, 0x6A);         // I2C device address 0x6A
-float accelX, accelY, accelZ,          // units m/s/s i.e. accelZ if often 9.8 (gravity)
-  gyroX, gyroY, gyroZ,                 // units dps (degrees per second)
-  gyroDriftX, gyroDriftY, gyroDriftZ;  // units dps
-float   gyroRoll, gyroPitch, gyroYaw,                             // units degrees (expect major drift)
+// LSM6DS3 myIMU(I2C_MODE, 0x6A);         // I2C device address 0x6A
+float accelX, accelY, accelZ,                               // units m/s/s i.e. accelZ if often 9.8 (gravity)
+  gyroX, gyroY, gyroZ,                                      // units dps (degrees per second)
+  gyroDriftX, gyroDriftY, gyroDriftZ;                       // units dps
+float gyroRoll, gyroPitch, gyroYaw,                         // units degrees (expect major drift)
   gyroCorrectedRoll, gyroCorrectedPitch, gyroCorrectedYaw,  // units degrees (expect minor drift)
   accRoll, accPitch, accYaw,                                // units degrees (roll and pitch noisy, yaw not possible)
   complementaryRoll, complementaryPitch, complementaryYaw;  // units degrees (excellent roll, pitch, yaw minor drift)
@@ -15,13 +17,19 @@ float   gyroRoll, gyroPitch, gyroYaw,                             // units degre
 #define MOUSE_RIGHT D8
 #define MOUSE_ACTIVATE D6
 const float accelerationThreshold = 1.6;  // threshold of significant in G's
-const int numSamples = 416;
+const int numSamples = 150;
+double samples[numSamples][9];
 int samplesRead = 0;
 int startTime, currentTime;
 uint8_t readData;
+
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+//                                   id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   // while (!Serial)
   //   ;
   pinMode(LED_BLUE, OUTPUT);
@@ -46,47 +54,62 @@ void setup() {
   // digitalWrite(LED_CHARGER, LOW);
 
   // Call .begin() to configure the IMUs
-  if (myIMU.begin() != 0) {
-    Serial.println("Device error");
+  // if (myIMU.begin() != 0) {
+  //   Serial.println("Device error");
+  //   while (1)
+  //     ;
+  // }
+  // Wire1.setClock(400000UL);  // SCL 400kHz  //
+  // Wire.setClock(400000UL);  // SCL 400kHz
+  startTime = micros();
+  calibrateIMU(0, 0);
+
+  if (!bno.begin()) {
+    Serial.print("No BNO055 detected");
     while (1)
       ;
   }
-  Wire1.setClock(400000UL);  // SCL 400kHz
-  startTime = micros();
-  calibrateIMU(0, 0);
 }
 
 void calibrateIMU(int delayMillis, int calibrationMillis) {
-  gyroDriftX = 0.67;
-  gyroDriftY = -1.38;
-  gyroDriftZ = -0.51;
+  gyroDriftX = 0;  //0.67;
+  gyroDriftY = 0;  //-1.38;
+  gyroDriftZ = 0;  //-0.51;
   return;
 }
 
-bool readIMU() {
-  uint8_t readData;
-  uint8_t data[12];
-  myIMU.readRegisterRegion(data, 0x22, 12);
-  int16_t *p = (int16_t *)data;
-  gyroX = *p * 2000.0 / 32768.0;
-  p++;
-  gyroY = *p * 2000.0 / 32768.0;
-  p++;
-  gyroZ = *p * 2000.0 / 32768.0;
-  p++;
-  accelX = *p * 4.0 / 32768.0;
-  p++;
-  accelY = *p * 4.0 / 32768.0;
-  p++;
-  accelZ = *p * 4.0 / 32768.0;
+// bool readIMU() {
+//   uint8_t readData;
+//   uint8_t data[12];
+//   myIMU.readRegisterRegion(data, 0x22, 12);
+//   int16_t *p = (int16_t *)data;
+//   gyroX = *p * 2000.0 / 32768.0;
+//   p++;
+//   gyroY = *p * 2000.0 / 32768.0;
+//   p++;
+//   gyroZ = *p * 2000.0 / 32768.0;
+//   p++;
+//   accelX = *p * 4.0 / 32768.0;
+//   p++;
+//   accelY = *p * 4.0 / 32768.0;
+//   p++;
+//   accelZ = *p * 4.0 / 32768.0;
 
+//   return true;
+// }
+sensors_event_t orientationData, linearAccelData, angVelData;
+bool readIMU() {
+
+  // bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+  bno.getEvent(&angVelData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
   return true;
 }
 
 int count = 0;
 float sumX, sumY, sumZ;
 int t1 = 0;
-
+#define PRECISION 4
 void loop() {
   int currentTime = millis();
   if (currentTime < t1 + 2000) {
@@ -101,10 +124,16 @@ void loop() {
   samplesRead = 0;
   while (samplesRead < numSamples) {
 wait:
-    myIMU.readRegister(&readData, LSM6DS3_ACC_GYRO_STATUS_REG);  //0,0,0,0,0,TDA,GDA,XLDA
-    if ((readData & 0x07) != 0x07) goto wait;
+
+    // myIMU.readRegister(&readData, LSM6DS3_ACC_GYRO_STATUS_REG);  //0,0,0,0,0,TDA,GDA,XLDA
+    // if ((readData & 0x07) != 0x07) goto wait;
     readIMU();
-    doCalculations();
+    // if (abs(linearAccelData.acceleration.x) + abs(linearAccelData.acceleration.y) + abs(linearAccelData.acceleration.z) < 0.2) {
+    //   delay(0.1);
+    //   goto wait;
+    // }
+    delay(10);
+    // doCalculations();
     // count++;
     // sumX += gyroX;
     // sumY += gyroY;
@@ -138,33 +167,53 @@ wait:
     // check if the all the required samples have been read since
     // the last time the significant motion was detected
 
-#define PRECISION 5
+
+    //   Serial.print(',');
+    //   Serial.print(gyroX - gyroDriftX, PRECISION);
+    //   Serial.print(',');
+    //   Serial.print(gyroY - gyroDriftY, PRECISION);
+    //   Serial.print(',');
+    //   Serial.print(gyroZ - gyroDriftZ, PRECISION);
+    //   Serial.print(',');
+    // Serial.print(gyroCorrectedRoll);
+    // Serial.print(',');
+    // Serial.print(gyroCorrectedPitch);
+    // Serial.print(',');
+    // Serial.println(gyroCorrectedYaw);
+    samples[samplesRead][0] = linearAccelData.acceleration.x;
+    samples[samplesRead][1] = linearAccelData.acceleration.y;
+    samples[samplesRead][2] = linearAccelData.acceleration.z;
+    samples[samplesRead][3] = angVelData.gyro.x;
+    samples[samplesRead][4] = angVelData.gyro.y;
+    samples[samplesRead][5] = angVelData.gyro.z;
+    // samples[samplesRead][6] = orientationData.orientation.heading;
+    // samples[samplesRead][7] = orientationData.orientation.roll;
+    // samples[samplesRead][8] = orientationData.orientation.pitch;
+
+    samplesRead++;
+    // if (samplesRead == numSamples) {
+    //   // add an empty line if it's the last sample
+    //   Serial.println();
+    //   break;
+    // }
+  }
+
+  samplesRead = 0;
+  while (samplesRead < numSamples) {
+
     // read the acceleration and gyroscope data
     // print the data in CSV format
-    Serial.print(accelX, PRECISION);
-    Serial.print(',');
-    Serial.print(accelY, PRECISION);
-    Serial.print(',');
-    Serial.print(accelZ, PRECISION);
-    Serial.print(',');
-    Serial.print(gyroX - gyroDriftX, PRECISION);
-    Serial.print(',');
-    Serial.print(gyroY - gyroDriftY, PRECISION);
-    Serial.print(',');
-    Serial.print(gyroZ - gyroDriftZ, PRECISION);
-    Serial.print(',');
-  Serial.print(gyroCorrectedRoll);
-  Serial.print(',');
-  Serial.print(gyroCorrectedPitch);
-  Serial.print(',');
-  Serial.println(gyroCorrectedYaw);
-    samplesRead++;
-    if (samplesRead == numSamples) {
-      // add an empty line if it's the last sample
-      Serial.println();
-      break;
+    // Serial.print(samplesRead);
+    // Serial.print(',');
+    for (int i = 0; i < 5; i++) {
+      Serial.print(samples[samplesRead][i], PRECISION);
+      Serial.print(',');
     }
+    Serial.println(samples[samplesRead][5], PRECISION);
+    samplesRead++;
   }
+  Serial.println();
+
   digitalWrite(LED_BLUE, LOW);
 }
 
