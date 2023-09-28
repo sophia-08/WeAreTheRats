348792bc3623267adce4ad59e19e030ddb8f920e
Sophia Dai <sophiadai8@gmail.com>
2023-09-28 09:34:46 -0700

    save each sample to seprate file
    python save.py  ch  startindex

--- capture/save.py
+++ capture/save.py
@@ -1,10 +1,20 @@
 import serial
 import time
+import sys
 
 # Define the UART device and file path
 uart_device = "/dev/cu.usbmodem14601"
 file_path = "capture.csv"
+file_index = 0
 num_samples=150
+if len(sys.argv) == 3:
+    # print(sys.argv[1],sys.argv[2] )
+    file_path = "./data/" + sys.argv[1]
+    file_index = int(sys.argv[2])
+else:
+    print ("wrong args")
+    exit(1)
+
 
 # Open the UART device for reading by line
 try:
@@ -16,43 +26,65 @@ except serial.SerialException as e:
 # Initialize a list to store the lines
 lines = []
 samples=0
-# Write the header to the file
-with open(file_path, "w") as file:
-    file.write("aX,aY,aZ,gX,gY,gZ\n")
+file = open(file_path + "_" + str(file_index) + ".dat", "w")
 
 try:
-    start_time = None
-
     while True:
         line = ser.readline().decode("utf-8").strip()
 
+
         # Only append non-empty lines
         if line:
-            print(".", end='')
-            lines.append(line)
-
-            if start_time is None:
-                start_time = time.time()
-
-        # Check if 2 seconds have passed since the first non-empty line was read
-        if start_time is not None and time.time() - start_time >= 2:
-            if len(lines) == num_samples:
-                # Save the data to the file
-                with open(file_path, "a") as file:
-                    file.write("\n".join(lines) + "\n")
-                    file.write("\n")
-                samples += 1
-                print(samples, " samples")
+            columns = line.split(',')        
+            print(columns[0])
+            # lines.append(line)
+            if (int(columns[0]) == 0) :
+                if file:
+                    file.close()
+                file = open(file_path + "_" + str(file_index) + ".dat", "w")
+                file_index += 1
+                file.write("lineno,aX,aY,aZ,gX,gY,gZ\n")
+                file.write(line + "\n")
             else:
-                print("Error: Incorrect number of lines read", len(lines))
+                file.write(line + "\n")
 
-            # Clear the lines list and reset the start time
-            lines = []
-            start_time = None
+        # Write the header to the file
+
+        # start_time = None
+
+        # while True:
+        #     line = ser.readline().decode("utf-8").strip()
+
+        #     # Only append non-empty lines
+        #     if line:
+        #         print(line[0:3])
+        #         # lines.append(line)
+        #         file.write(line + "\n")
+
+            #     if start_time is None:
+            #         start_time = time.time()
+
+            # # Check if 2 seconds have passed since the first non-empty line was read
+            # if start_time is not None and time.time() - start_time >= 2:
+            #     if len(lines) == num_samples:
+            #         # Save the data to the file
+            #         with open(file_path, "a") as file:
+            #             file.write("\n".join(lines) + "\n")
+            #             file.write("\n")
+            #         samples += 1
+            #         print(samples, " samples")
+            #     else:
+            #         print("Error: Incorrect number of lines read", len(lines))
+
+            #     # Clear the lines list and reset the start time
+            #     lines = []
+            #     start_time = None
 
 except KeyboardInterrupt:
     # Close the UART port when the user presses Ctrl+C
     ser.close()
+    if file:
+        file.close()
     print("UART port closed.")
 except Exception as e:
     # Handle other exceptions
@@ -60,6 +92,8 @@ except Exception as e:
 finally:
     # Close the UART port and save any remaining data if an error occurs
     ser.close()
+    if file:
+        file.close()
     # with open(file_path, "a") as file:
     #     file.write("\n".join(lines) + "\n")
     print("UART port closed. Remaining data saved to capture.txt")
