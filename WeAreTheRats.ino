eb41e1d0fb595775430fa12f543e50535cbf9d1f
Sophia Dai <sophiadai8@gmail.com>
2023-10-04 09:49:09 -0700

    Use tom to switch device mode

--- WeAreTheRats.ino
+++ WeAreTheRats.ino
@@ -100,12 +100,6 @@ int ledred = 0;
 
 void setup() {
   Serial.begin(115200);
-  // while (!Serial)
-  //   ;
-  while (!Serial) delay(1);  // for nrf52840 with native usb
-
-  Serial.println("We are the rats\n");
-  Serial.println("-------------------------------------\n");
 
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_RED, OUTPUT);
@@ -114,6 +108,21 @@ void setup() {
   pinMode(IMU_RESET, OUTPUT);
   pinMode(DEBUG_2, OUTPUT);
   pinMode(DEBUG_3, OUTPUT);
+
+
+// Mystery of why !Serial not ready:
+// The "Serial" is always valid for an Arduino Uno, therefor that piece of code does not wait.
+// In the Leonardo, the "Serial" could be zero, if the serial monitor has not been opened yet.
+  // while (!Serial) {
+  //   digitalWrite(LED_RED, LIGHT_ON);
+  //   delay(10);
+  //   digitalWrite(LED_RED, LIGHT_OFF);
+  //   delay(100);
+  // }
+
+  Serial.println("We are the rats\n");
+  Serial.println("-------------------------------------\n");
+
   //Reset IMU
   digitalWrite(IMU_RESET, HIGH);
   digitalWrite(IMU_RESET, LOW);
@@ -160,15 +169,15 @@ void setup() {
   // SRAM usage required by SoftDevice will increase with number of connections
   Bluefruit.begin(1, 1);
   // HID Device can have a min connection interval of 9*1.25 = 11.25 ms
-  Bluefruit.Periph.setConnInterval(9, 16);  // min = 9*1.25=11.25 ms, max = 16*1.25=20ms
-Bluefruit.Central.setConnInterval(9, 16);  // min = 9*1.25=11.25 ms, max = 16*1.25=20ms
+  Bluefruit.Periph.setConnInterval(9, 16);   // min = 9*1.25=11.25 ms, max = 16*1.25=20ms
+  Bluefruit.Central.setConnInterval(9, 16);  // min = 9*1.25=11.25 ms, max = 16*1.25=20ms
 
 
   // Callbacks for Central
   Bluefruit.Central.setConnectCallback(cent_connect_callback);
   Bluefruit.Central.setDisconnectCallback(cent_disconnect_callback);
 
-  Bluefruit.setTxPower(4);                  // Check bluefruit.h for supported values
+  Bluefruit.setTxPower(4);  // Check bluefruit.h for supported values
   Bluefruit.setName("WeAreTheRats");
 
   // Configure and Start Device Information Service
@@ -190,10 +199,10 @@ Bluefruit.Central.setConnInterval(9, 16);  // min = 9*1.25=11.25 ms, max = 16*1.
    */
   Bluefruit.Scanner.setRxCallback(scan_callback);
   Bluefruit.Scanner.restartOnDisconnect(true);
-  Bluefruit.Scanner.setInterval(160, 80); // in unit of 0.625 ms
+  Bluefruit.Scanner.setInterval(160, 80);  // in unit of 0.625 ms
   Bluefruit.Scanner.filterUuid(BLEUART_UUID_SERVICE);
   Bluefruit.Scanner.useActiveScan(false);
-  Bluefruit.Scanner.start(0);                   // 0 = Don't stop scanning after n seconds
+  Bluefruit.Scanner.start(0);  // 0 = Don't stop scanning after n seconds
 
   // Set up and start advertising
   startAdv();
@@ -737,41 +746,37 @@ wait:
 /*------------------------------------------------------------------*/
 /* Central
  *------------------------------------------------------------------*/
-void scan_callback(ble_gap_evt_adv_report_t* report)
-{
+void scan_callback(ble_gap_evt_adv_report_t *report) {
   // Since we configure the scanner with filterUuid()
-  // Scan callback only invoked for device with bleuart service advertised  
-  // Connect to the device with bleuart service in advertising packet  
+  // Scan callback only invoked for device with bleuart service advertised
+  // Connect to the device with bleuart service in advertising packet
   Bluefruit.Central.connect(report);
 }
 
-void cent_connect_callback(uint16_t conn_handle)
-{
+void cent_connect_callback(uint16_t conn_handle) {
   // Get the reference to current connection
-  BLEConnection* connection = Bluefruit.Connection(conn_handle);
+  BLEConnection *connection = Bluefruit.Connection(conn_handle);
 
   char peer_name[32] = { 0 };
   connection->getPeerName(peer_name, sizeof(peer_name));
 
   Serial.print("[Cent] Connected to ");
-  Serial.println(peer_name);;
+  Serial.println(peer_name);
+  ;
 
-  if ( clientUart.discover(conn_handle) )
-  {
+  if (clientUart.discover(conn_handle)) {
     // Enable TXD's notify
     clientUart.enableTXD();
-  }else
-  {
+  } else {
     // disconnect since we couldn't find bleuart service
     Bluefruit.disconnect(conn_handle);
-  }  
+  }
 }
 
-void cent_disconnect_callback(uint16_t conn_handle, uint8_t reason)
-{
-  (void) conn_handle;
-  (void) reason;
-  
+void cent_disconnect_callback(uint16_t conn_handle, uint8_t reason) {
+  (void)conn_handle;
+  (void)reason;
+
   Serial.println("[Cent] Disconnected");
 }
 
@@ -780,24 +785,23 @@ void cent_disconnect_callback(uint16_t conn_handle, uint8_t reason)
  * @param cent_uart Reference object to the service where the data 
  * arrived. In this example it is clientUart
  */
-void cent_bleuart_rx_callback(BLEClientUart& cent_uart)
-{
-  char str[20+1] = { 0 };
+void cent_bleuart_rx_callback(BLEClientUart &cent_uart) {
+  char str[20 + 1] = { 0 };
   cent_uart.read(str, 20);
-      
+
   Serial.print("[Cent] RX: ");
   Serial.println(str);
 
-  blehid.keyPress(str[0]);
-  needSendKeyRelease = true;
-  // if ( bleuart.notifyEnabled() )
-  // {
-  //   // Forward data from our peripheral to Mobile
-  //   bleuart.print( str );
-  // }else
-  // {
-  //   // response with no prph message
-  //   clientUart.println("[Cent] Peripheral role not connected");
-  // }  
-}
+  // blehid.keyPress(str[0]);
+  // needSendKeyRelease = true;
 
+  if (str[0] == 'a') {
+    if (deviceMode == DEVICE_MOUSE_MODE) {
+      deviceMode = DEVICE_KEYBOARD_MODE;
+      Serial.println("swithc to keyboard");
+    } else {
+      deviceMode = DEVICE_MOUSE_MODE;
+      Serial.println("swithc to mouse");
+    }
+  }
+}
