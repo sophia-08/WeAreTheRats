a5164e0423ed858e79e1bb5a9e4704bb6a173fcd
Sophia Dai <sophiadai8@gmail.com>
2023-10-18 23:32:03 -0700

    Move code out of main

---
+++ battery.cpp
@@ -0,0 +1,18 @@
+#include <bluefruit.h>
+#include "local_constants.h"
+#define VBAT_PER_LBS (0.003515625F) // 3.6 reference and 10 bit resolution
+
+float GetBatteryVoltage() {
+  // digitalWrite(VBAT_ENABLE, LOW);
+
+  uint32_t adcCount = analogRead(PIN_VBAT);
+  float adcVoltage = adcCount * VBAT_PER_LBS;
+  float vBat = adcVoltage * (1510.0 / 510.0);
+
+  // digitalWrite(VBAT_ENABLE, HIGH);
+
+  return vBat;
+}
+
+bool IsChargingBattery() { return digitalRead(BAT_CHARGE_STATE) == LOW; }
+
---
+++ local_constants.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#define MOUSE_LEFT D9
+#define MOUSE_RIGHT D8
+#define MOUSE_ACTIVATE D6
+#define IMU_RESET D0
+#define SWITCH_DEVICE_MODE D10
+#define KEYPAD_CENTER D10
+#define KEYPAD_UP D7
+#define KEYPAD_RIGHT D3
+#define KEYPAD_DOWN D2
+#define KEYPAD_LEFT D1
+
+// #define LED_CHARGER 23
+#define LIGHT_ON LOW
+#define LIGHT_OFF HIGH
+#define BAT_CHARGE_STATE 23 // LOW for charging, HIGH not charging
+
+// VBAT_ENABLE 14
+
+// #define DEBUG_2 D2
+// #define DEBUG_3 D3
+
+#define SMOOTHING_RATIO 0.8
+#define SENSITIVITY_X 30
+#define SENSITIVITY_Y 35
+#define DEBOUNCE_DELAY 5 // ms
+
+#define DEVICE_MOUSE_MODE 0
+#define DEVICE_KEYBOARD_MODE 1
