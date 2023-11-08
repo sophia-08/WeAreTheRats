a53e3180d056e3ee0b171973c774d05fd9556e0d
Sophia Dai <sophiadai8@gmail.com>
2023-11-07 23:24:04 -0800

    print out calibration status

--- system.cpp
+++ system.cpp
@@ -1,3 +1,5 @@
+#include "Adafruit_USBD_CDC.h"
+#include "nrf_soc.h"
 #include <bluefruit.h>
 #include "local_constants.h"
 
@@ -26,4 +28,11 @@ void systemSleep() {
 //   // power off
 //   sd_power_system_off();
 //   pinMode(DEVICE_ACTIVATE, INPUT_PULLUP);
+  // nrf_gpio_cfg_sense_input(g_ADigitalPinMap[IMU_INT],
+  //                          NRF_GPIO_PIN_PULLUP, NRF_GPIO_PIN_SENSE_LOW);
+// sd_power_mode_set(NRF_POWER_MODE_LOWPWR);
+// // nrf_pwr_mgmt_run();
+// attachInterrupt(IMU_INT, myinthandler, FALLING);  //RISING
+// sd_app_evt_wait();
+// pinMode(IMU_INT, INPUT_PULLUP);
 }
