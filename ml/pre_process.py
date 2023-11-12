86b1747d80c1543adf24d2c3a0864ae160037a71
Sophia Dai <sophiadai8@gmail.com>
2023-11-11 17:10:20 -0800

    print out stastics info

--- ml/pre_process.py
+++ ml/pre_process.py
@@ -10,11 +10,16 @@ file_path = "../capture_bno085/data/*.dat"
 file_index = 0
 
 out_samples = 150
+file_counts = {chr(i): 0 for i in range(ord('a'), ord('z') + 1)}
 
 
 #                  i            j            k            r           aX           aY           aZ           gX           gY           gZ           rx           ry           rz
 # min      -0.791140    -0.725280    -0.999940     0.000000   -21.003910   -27.644530   -15.917970   -12.785160    -6.890620    -8.861330    -1.218855    -1.171420    -2.541700
 # max       0.528630     0.721860     0.999820     0.999940    27.953120     6.628910    24.125000    10.705080     9.562500     8.347660     1.711840     1.518310     1.513890
+
+
+# min      -0.791140    -0.725280    -0.999940     0.000000   -21.003910   -28.523440   -17.769530   -13.460940    -6.890620   -11.802730    -1.218855    -1.171420    -2.591830
+# max       0.668150     0.736880     0.999820     1.000000    33.292970     9.371090    28.054690    11.369140     9.562500     9.562500     1.858615     1.518310     1.513890
 
 accl_min = -30.0
 accl_max = 30.0
@@ -48,7 +53,7 @@ for datafile in datafiles:
     # To start with, take all samples
 
     print("Total points: ", len(df))
-    if len(df) < 60:
+    if len(df) < 45:
         # too few points, ignore
         continue
 
@@ -86,9 +91,10 @@ for datafile in datafiles:
         zeros_df = pd.DataFrame(zeros_data)
 
         df = pd.concat([df, zeros_df], ignore_index=True)
-        
+
     saveto = "processed_" + datafile[18:]
     df.drop(["i", "j", "k","r"], axis=1, inplace=True)
+    file_counts[datafile[23]] = file_counts[datafile[23]] + 1
 
     df['aX'] = (df['aX'] - accl_min) / (accl_max - accl_min)
     df['aY'] = (df['aY'] - accl_min) / (accl_max - accl_min)
@@ -106,6 +112,7 @@ for datafile in datafiles:
 
 print(mindf.describe())
 print(maxdf.describe())
+print(file_counts)
 
 
 pass
