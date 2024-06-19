53e3bf190dfb63154f85392c067ed41fbd5f420c
Sophia Dai <sophiadai8@gmail.com>
2024-06-19 11:01:19 -0700

    Add Makefile

--- test/hid/mouse_mover/mouse_mover.cpp
+++ test/hid/mouse_mover/mouse_mover.cpp
@@ -1,29 +1,28 @@
-#include <iostream>
-#include <chrono>
-#include <thread>
 #include <ApplicationServices/ApplicationServices.h>
+#include <chrono>
+#include <iostream>
+#include <thread>
 
 void moveMouse(int dx, int dy) {
-    CGEventRef event = CGEventCreate(nullptr);
-    CGPoint currentPos = CGEventGetLocation(event);
-    CFRelease(event);
+  CGEventRef event = CGEventCreate(nullptr);
+  CGPoint currentPos = CGEventGetLocation(event);
+  CFRelease(event);
 
-    CGPoint newPos = CGPointMake(currentPos.x + dx, currentPos.y + dy);
-    CGEventRef moveEvent = CGEventCreateMouseEvent(
-        nullptr, kCGEventMouseMoved, newPos, kCGMouseButtonLeft
-    );
+  CGPoint newPos = CGPointMake(currentPos.x + dx, currentPos.y + dy);
+  CGEventRef moveEvent = CGEventCreateMouseEvent(nullptr, kCGEventMouseMoved,
+                                                 newPos, kCGMouseButtonLeft);
 
-    CGEventPost(kCGHIDEventTap, moveEvent);
-    CFRelease(moveEvent);
+  CGEventPost(kCGHIDEventTap, moveEvent);
+  CFRelease(moveEvent);
 }
 
 int main() {
-    std::cout << "Starting mouse mover. Press Ctrl+C to exit." << std::endl;
+  std::cout << "Starting mouse mover. Press Ctrl+C to exit." << std::endl;
 
-    while (true) {
-        moveMouse(50, 50);
-        std::this_thread::sleep_for(std::chrono::seconds(30));
-    }
+  while (true) {
+    moveMouse(50, 50);
+    std::this_thread::sleep_for(std::chrono::seconds(30));
+  }
 
-    return 0;
+  return 0;
 }
