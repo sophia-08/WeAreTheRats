2c946c900f85a6c1b32eba0c06350a59fd85b835
Sophia Dai <sophiadai8@gmail.com>
2024-07-17 10:12:21 -0700

    Print out length

--- test/mac_report.cpp
+++ test/mac_report.cpp
@@ -29,7 +29,7 @@ public:
   static void inputReportCallback(void *context, IOReturn result, void *sender,
                                   IOHIDReportType type, uint32_t reportID,
                                   uint8_t *report, CFIndex reportLength) {
-    std::cout << "Received report: ";
+    std::cout << "Received report: " << reportLength << " bytes, ";
     for (CFIndex i = 0; i < reportLength; i++) {
       printf("%02X ", report[i]);
     }
@@ -65,19 +65,19 @@ public:
     conclusion:
       only id 1, which is keyboard, has output report channel.
     */
-    int id = 2;
-    // for (id=0; id<4; id++)
-    {
-      std::cerr << "send two bytes to " << id << std::endl;
-      std::vector<uint8_t> out = {
-          0x01, 0x02, 0x03, 0x04, 0x05, 6, 7, 8, 9,
-      };
-      device->sendReport(id, out);
-    }
+    // int id = 2;
+    // // for (id=0; id<4; id++)
+    // {
+    //   std::cerr << "send two bytes to " << id << std::endl;
+    //   std::vector<uint8_t> out = {
+    //       0x01, 0x02, 0x03, 0x04, 0x05, 6, 7, 8, 9,
+    //   };
+    //   device->sendReport(id, out);
+    // }
   }
 
   void registerInputReportCallback() {
-    static uint8_t report[64]; // Adjust size as needed
+    static uint8_t report[128]; // Adjust size as needed
     IOHIDDeviceRegisterInputReportCallback(m_device, report, sizeof(report),
                                            inputReportCallback,
                                            this); // Pass 'this' as context
