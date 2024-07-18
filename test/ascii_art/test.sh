cef40109a495c94996cc7d598d717c7f95c5862d
Sophia Dai <sophiadai8@gmail.com>
2024-07-18 16:26:38 -0700

    Banner

---
+++ test/ascii_art/test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash        
+
+# Define color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLINK='\033[5m'
+BOLD='\033[1m'
+NC='\033[0m' # No Color
+# echo 'Your string with '\"'\"'single quotes'\"'\"' inside'
+
+echo -e "             .--,       .--,                                                "         
+echo -e "            ( (  \.---./  ) )                            ___                "            
+echo -e "             '.__/o   o\__.'                           .'o O'-._            "            
+echo -e "                {=  ^  =}                             / O o_.-\`|            "              
+echo -e "                 >  -  <                             /O_.-'  O |            "        
+echo -e "  ___________.\"\"\`-------\`\"\".____________            | o   o .-\`         "           
+echo -e " / Find me on github:                0  \\           |o O_.-'                "             
+echo -e " \\  Ring:                      o        /           '--\`                    "           
+echo -e " /    sophia-08/WeAreTheRats            \\                                   "          
+echo -e " \\  Companion App:                O     /         __                        "        
+echo -e " /    sophia-08/HereComesTheChees       \     _.-'  \`.                      "          
+echo -e " \\______________o__________o____________/ .-~^        \`~--'                 "        
+echo -e "               ___)( )(___        \`-.___.'                                  "         
+echo -e "             (((__)  (__)))                                                 "             
+
+echo -e "\n\n"
+
+echo -e "             .--,       .--,                                                "         
+echo -e "            ( (  \.---./  ) )                            ___                "            
+echo -e "             '.__/${RED}o   o${NC}\__.'                           .'o O'-._            "            
+echo -e "                {=  ${RED}^${NC}  =}                             / O o_.-\`|            "              
+echo -e "                 >  ${RED}-${NC}  <                             /O_.-'  O |            "        
+echo -e "  ___________.\"\"\`-------\`\"\".____________            | o   o .-\`         "           
+echo -e " / Find me on github:                0  \\           |o O_.-'                "             
+echo -e " \\  Ring:                      o        /           '--\`                    "           
+echo -e " /    ${GREEN}sophia-08/WeAreTheRats${NC}            \\                                   "          
+echo -e " \\  Companion App:                O     /         __                        "        
+echo -e " /    ${GREEN}sophia-08/HereComesTheChees${NC}       \     _.-'  \`.                      "          
+echo -e " \\______________o__________o____________/ .-~^        \`~--'                 "        
+echo -e "               ___)( )(___        \`-.___.'                                  "         
+echo -e "             (((__)  (__)))                                                 "             
+
+echo -e "\n\n"
+printf "\n            .--,       .--,                                         "       
+printf "\n           ( (  \.---./  ) )                            ___         "      
+printf "\n            '.__/${RED}-   -${NC}\__.'                           .'o O'-._     "           
+printf "\n               {=  ${RED}${BLINK}${BOLD}^${NC}  =}                             / O o_.-\`|     "          
+printf "\n                >  ${RED}x${NC}  <                             /O_.-'  O |     "               
+printf "\n ___________.\"\"\`-------\`\"\".____________            | o   o .-\`      "               
+printf "\n/ Find me on github:                0  \\           |o O_.-\'         "                 
+printf "\n\\  Ring:                      o        /           '--\`             "           
+printf "\n/    sophia-08/WeAreTheRats            \\                            "              
+printf "\n\  Companion App:                O     /         __                 "           
+printf "\n/    sophia-08/HereComesTheChees       \\     _.-'  \`.               "          
+printf "\n\\______________o__________o____________/ .-~^        \`~--'          "            
+printf "\n              ___)( )(___        \`-.___.'                           "      
+printf "\n            (((__)  (__)))                                          " 
+
+
