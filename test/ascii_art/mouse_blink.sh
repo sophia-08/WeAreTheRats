11c7d3dfeabd6846a32e1a196d5ba17964afc688
Sophia Dai <sophiadai8@gmail.com>
2024-08-10 14:06:43 -0700

    Blink mouse

---
+++ test/ascii_art/mouse_blink.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# clear
+
+# while true; do
+#     for eyes in "o   o" "O   O"; do
+#         clear
+#         echo -e "             .--,       .--,                                                "         
+#         echo -e "            ( (  \\.---./  ) )                            ___                "            
+#         echo -e "             '.__/${eyes}\\__.'                           .'o O'-._            "            
+#         echo -e "                {=  ^  =}                             / O o_.-\`|            "              
+#         echo -e "                 >  -  <                             /O_.-'  O |            "        
+#         echo -e "  ___________.\"\"\`-------\`\"\".____________            | o   o .-\`         "           
+#         echo -e " / Find me on github:                0  \\           |o O_.-'                "             
+#         echo -e " \\  Ring:                      o        /           '--\`                    "           
+#         echo -e " /    sophia-08/WeAreTheRats            \\                                   "          
+#         echo -e " \\  Companion App:                O     /         __                        "        
+#         echo -e " /    sophia-08/HereComesTheChees       \\     _.-'  \`.                      "          
+#         echo -e " \\______________o__________o____________/ .-~^        \`~--'                 "        
+#         echo -e "               ___)( )(___        \`-.___.'                                  "         
+#         echo -e "             (((__)  (__)))                                                 "
+#         sleep 0.5
+#     done
+# done
+
+
+#!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m'  # No Color
+
+clear
+
+while true; do
+    for eyes in "-   -" "O   O" "O   O" "O   O" "o   o" ; do
+        clear
+        echo -e "             .--,       .--,                                                "         
+        echo -e "            ( (  \\.---./  ) )                            ___                "            
+        echo -e "             '.__/${RED}${eyes}${NC}\\__.'                           .'o O'-._            "            
+        echo -e "                {=  ${RED}^${NC}  =}                             / O o_.-\`|            "              
+        echo -e "                 >  ${RED}-${NC}  <                             /O_.-'  O |            "        
+        echo -e "  ___________.\"\"\`-------\`\"\".____________            | o   o .-\`         "           
+        echo -e " / Find me on github:                0  \\           |o O_.-'                "             
+        echo -e " \\  Ring:                      o        /           '--\`                    "           
+        echo -e " /    sophia-08/WeAreTheRats            \\                                   "          
+        echo -e " \\  Companion App:                O     /         __                        "        
+        echo -e " /    sophia-08/HereComesTheChees       \\     _.-'  \`.                      "          
+        echo -e " \\______________o__________o____________/ .-~^        \`~--'                 "        
+        echo -e "               ___)( )(___        \`-.___.'                                  "         
+        echo -e "             (((__)  (__)))                                                 "
+        sleep 0.5
+    done
+done
+
+RED='\033[0;31m'; NC='\033[0m'; while true; do for eyes in "-   -" "O   O" "O   O" "O   O" "o   o"; do clear; echo -e "             .--,       .--,                                                \n            ( (  \\.---./  ) )                            ___                \n             '.__/${RED}${eyes}${NC}\\__.'                           .'o O'-._            \n                {=  ^  =}                             / O o_.-\`|            \n                 >  -  <                             /O_.-'  O |            \n  ___________.\"\"\`-------\`\"\".____________            | o   o .-\`         \n / Find me on github:                0  \\           |o O_.-'                \n \\  Ring:                      o        /           '--\`                    \n /    sophia-08/WeAreTheRats            \\                                   \n \\  Companion App:                O     /         __                        \n /    sophia-08/HereComesTheChees       \\     _.-'  \`.                      \n \\______________o__________o____________/ .-~^        \`~--'                 \n               ___)( )(___        \`-.___.'                                  \n             (((__)  (__)))                                                 "; sleep 0.5; done; done
+
