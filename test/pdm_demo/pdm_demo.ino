03535b54f9ae2e7113731c81485f531dbebc3618
Sophia Dai <sophiadai8@gmail.com>
2024-09-28 18:11:47 -0700

    Read 4000 PDM sample and output amplitude

--- test/pdm_demo/pdm_demo.ino
+++ test/pdm_demo/pdm_demo.ino
@@ -11,14 +11,16 @@
 #include <PDM.h>
 
 // buffer to read samples into, each sample is 16-bits
-short sampleBuffer[256];
+short sampleBuffer[4000];
+int32_t mic;
 
 // number of samples read
 volatile int samplesRead;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) yield();
+  while (!Serial)
+    yield();
 
   // configure the data receive callback
   PDM.onReceive(onPDMdata);
@@ -31,20 +33,28 @@ void setup() {
   // - a 16 kHz sample rate
   if (!PDM.begin(1, 16000)) {
     Serial.println("Failed to start PDM!");
-    while (1) yield();
+    while (1)
+      yield();
   }
 }
 
 void loop() {
   // wait for samples to be read
-  if (samplesRead) {
-    // print samples to the serial monitor or plotter
-    for (int i = 0; i < samplesRead; i++) {
-      Serial.println(sampleBuffer[i]);
-    }
-    // clear the read count
-    samplesRead = 0;
-  }
+  //   if (samplesRead) {
+  //     // print samples to the serial monitor or plotter
+  //     for (int i = 0; i < samplesRead; i++) {
+  //       Serial.println(sampleBuffer[i]);
+  //     }
+  //     // clear the read count
+  //     samplesRead = 0;
+  //   }
+  samplesRead = 0;
+  mic = getPDMwave(4000);
+
+  Serial.print("Mic: ");
+  Serial.println(mic);
+
+  // while(1);
 }
 
 void onPDMdata() {
@@ -58,3 +68,22 @@ void onPDMdata() {
   samplesRead = bytesAvailable / 2;
 }
 
+int32_t getPDMwave(int32_t samples) {
+  short minwave = 30000;
+  short maxwave = -30000;
+
+  while (samples > 0) {
+    if (!samplesRead) {
+      yield();
+      continue;
+    }
+    for (int i = 0; i < samplesRead; i++) {
+      minwave = min(sampleBuffer[i], minwave);
+      maxwave = max(sampleBuffer[i], maxwave);
+      samples--;
+    }
+    // clear the read count
+    samplesRead = 0;
+  }
+  return maxwave - minwave;
+}
