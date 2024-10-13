b1117bcdc4ddb07d551810954af73a9f1713c31a
Sophia Dai <sophiadai8@gmail.com>
2024-10-13 11:47:40 -0700

    Fix mouse move direction

--- v3/imu.cpp
+++ v3/imu.cpp
@@ -41,13 +41,9 @@ int imuReadAndUpdateXYAngle() {
   readIMU();
   doCalculations();
   // printOrientation();
-#ifdef PIMORONI_TRACKBALL
   xAngle = Orientation[0];
   yAngle = -Orientation[2];
-#else
-  xAngle = -Orientation[2];
-  yAngle = Orientation[0];
-#endif
+if
   if (bWrite) {
     writeTrajectory[writeIndex][0] = xAngle;
     writeTrajectory[writeIndex][1] = yAngle;
